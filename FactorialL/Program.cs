4fb98d14832573f470b5f1be9991179fcb66c0cd
Александр Ломтев <allomtiev@gmail.com>
2020-09-08 19:05:04 +0300

    Добавьте файлы проекта.

---
+++ FactorialL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactorialL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(9));
+        }
+        public static long Fact(int number)
+        {
+            if (number >= 1)
+            {
+                long result = 1;
+                do
+                {
+                    result = result * number;
+                    number--;
+                }
+                while (number != 1);
+                return result;
+            }
+            return 0;
+        }
+    }
+}
